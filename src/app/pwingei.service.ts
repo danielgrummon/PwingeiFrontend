3ad81de328ceeaebe3297ca5d2ba62f5b382b12d
Daniel Grummon <grummon1@hotmail.com>
2020-12-19 13:46:23 -0500

    added back end service

--- src/app/pwingei.service.ts
+++ src/app/pwingei.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
-import { catchError, tap, map } from 'rxjs/operators';
 import { IPhenotype } from './list/list.model';
+import { catchError, tap } from 'rxjs/operators';
 
 
 @Injectable({
@@ -10,35 +10,33 @@ import { IPhenotype } from './list/list.model';
 })
 export class PwingeiService {
 
-  // private productUrl = './assets/pwingei/pwingei.json';
+  private phenotypeListUrl = 'https://pwingei-backend.herokuapp.com/app/rest/wingei/pwingei';
+  constructor(private httpClient: HttpClient) { }
 
-  // constructor(private http: HttpClient) { }
+  options = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    })
+  };
 
-  // getPhenotypes(): Observable<IPhenotype[]> {
-  //   return this.http.get<IPhenotype[]>(this.productUrl)
-  //     .pipe(
-  //       tap(data => console.log('All: ' + JSON.stringify(data))),
-  //       catchError(this.handleError)
-  //     );
-  // }
+  getPhenotypeList(): Observable<IPhenotype[]> {
+    console.log("service retrieving list");
+    return this.httpClient.get<IPhenotype[]>(this.phenotypeListUrl)
+      .pipe(
+        tap(data => console.log(JSON.stringify(data))),
+        catchError(this.handleError)
+      );
+  }
 
-  // getPhenotype(id: number): Observable<IPhenotype | undefined> {
-  //   return this.getPhenotypes()
-  //     .pipe(
-  //       map((phenotypes: IPhenotype[]) => phenotypes.find(p => p.id === id))
-  //     );
-  // }
-
-  // private handleError(err: HttpErrorResponse): Observable<never> {
-  //   let errorMessage = '';
-  //   if (err.error instanceof ErrorEvent) {
-
-  //     errorMessage = `An error occurred: ${err.error.message}`;
-  //   } else {
-
-  //     errorMessage = `Server returned code: ${err.status}, error message is: ${err.message}`;
-  //   }
-  //   console.error(errorMessage);
-  //   return throwError(errorMessage);
-  // }
+  private handleError(err: HttpErrorResponse) {
+    let errorMessage = '';
+    if (err.error instanceof ErrorEvent) {
+      errorMessage = `An error occurred: ${err.error.message}`;
+    } else {
+      errorMessage = `Server returned code: ${err.status}, error message is: ${err.message}`;
+    }
+    console.error(errorMessage);
+    return throwError(errorMessage);
+  }
 }
