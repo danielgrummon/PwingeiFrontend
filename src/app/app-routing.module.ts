20bbcc64df79d7b572e06873b9e07c42f629b215
Daniel Grummon <grummon1@hotmail.com>
2020-12-20 22:52:13 -0500

    adding content

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,16 @@ import { RouterModule, Routes } from '@angular/router';
 import { DetailComponent } from './detail/detail.component';
 import { ListComponent } from './list/list.component';
 import { WelcomeComponent } from './welcome/welcome.component';
+import { MinimalistComponent } from './minimalist/minimalist.component';
+import { HybridsComponent } from './hybrids/hybrids.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'welcome' },
   { path: 'welcome', component: WelcomeComponent },
   { path: 'list', component: ListComponent },
   { path: 'detail/:id', component: DetailComponent },
+  { path: 'minimalist', component: MinimalistComponent },
+  { path: 'hybrids', component: HybridsComponent },
   { path: '**', redirectTo: 'welcome', pathMatch: 'full' }
 ];
 
