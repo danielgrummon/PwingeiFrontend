afdc5caa2a5311c94c20b0afafdfec39e17a4076
Daniel Grummon <grummon1@hotmail.com>
2020-12-29 17:41:32 -0500

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { DetailComponent } from './detail/detail.component';
-import { ListComponent } from './list/list.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { MinimalistComponent } from './minimalist/minimalist.component';
 import { HybridsComponent } from './hybrids/hybrids.component';
 
-const routes: Routes = [
+const ROUTES: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'welcome' },
   { path: 'welcome', component: WelcomeComponent },
   { path: 'minimalist', component: MinimalistComponent },
@@ -16,7 +14,7 @@ const routes: Routes = [
 
 @NgModule({
   declarations: [],
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(ROUTES)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
