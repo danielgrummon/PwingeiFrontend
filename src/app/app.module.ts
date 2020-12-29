2845fd1f8a27a8248acf8bb1527f8cef1c717046
Daniel Grummon <grummon1@hotmail.com>
2020-12-28 19:09:00 -0500

    changed modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,34 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './welcome/welcome.component';
-import { ListComponent } from './list/list.component';
-import { PicturesComponent } from './pictures/pictures.component';
-import { TextComponent } from './text/text.component';
-import { DetailComponent } from './detail/detail.component';
-import { AppRoutingModule } from './app-routing.module';
-import { StatsComponent } from './stats/stats.component';
 import { MinimalistComponent } from './minimalist/minimalist.component';
 import { HybridsComponent } from './hybrids/hybrids.component';
+import { ListModule } from './list/list.module';
+import { RouterModule } from '@angular/router';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     WelcomeComponent,
-    ListComponent,
-    PicturesComponent,
-    TextComponent,
-    DetailComponent,
-    StatsComponent,
     MinimalistComponent,
     HybridsComponent
   ],
   imports: [
+    SharedModule,
     BrowserModule,
-    AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot([
+      { path: '', pathMatch: 'full', redirectTo: 'welcome' },
+      { path: 'welcome', component: WelcomeComponent },
+      { path: 'minimalist', component: MinimalistComponent },
+      { path: 'hybrids', component: HybridsComponent },
+      { path: '**', redirectTo: 'welcome', pathMatch: 'full' }
+    ]),
+    ListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
