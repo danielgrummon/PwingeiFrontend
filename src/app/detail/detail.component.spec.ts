ccea44b4ea165fea525985ce929aa1ca7ba7b615
danielgrummon <grummon1@hotmail.com>
2021-01-24 00:31:17 -0500

    added unit and integration tests

--- src/app/detail/detail.component.spec.ts
+++ src/app/detail/detail.component.spec.ts
@@ -1,25 +1,55 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ActivatedRoute, Router } from '@angular/router';
+import { of } from 'rxjs';
+import { PwingeiService } from '../pwingei.service';
 import { DetailComponent } from './detail.component';
+
+//component unit test
 
 describe('DetailComponent', () => {
   let component: DetailComponent;
+  let WINGEI_OBJECT;
   let fixture: ComponentFixture<DetailComponent>;
-
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ DetailComponent ]
-    })
-    .compileComponents();
-  }));
+  let mockActivatedRoute, mockPwingeiService;
+  let mockRouter = {
+    navigate: jasmine.createSpy('navigate')
+  };
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(DetailComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+    mockActivatedRoute = {
+      snapshot: { paramMap: { get: () => { return '1'; } } }
+    }
+
+    WINGEI_OBJECT = [
+      { id: 1, type: '7', location: 'Campoma Bridge', date: '2008', imageUrl: '/assets/images/campoma7s.jpg', imageUrl1: '/assets/images/campoma7.jpg', imageUrl2: '/assets/images/campoma7b.jpg', price: '35', name: 'Yellow Tail', available: 'NO', description: 'Add description here.' },
+    ]
+
+    mockPwingeiService = jasmine.createSpyObj(['getPhenotypeList', 'getPhenotypeById'])
+
+    TestBed.configureTestingModule({
+      declarations: [DetailComponent],
+      providers: [
+        { provide: ActivatedRoute, useValue: mockActivatedRoute },
+        { provide: Router, useValue: mockRouter },
+        { provide: PwingeiService, useValue: mockPwingeiService }
+      ]
+    });
+    fixture = TestBed.createComponent(DetailComponent);
+    mockPwingeiService.getPhenotypeById.and.returnValue(of(WINGEI_OBJECT))
+  })
+
+  it('should write a proper page title', () => {
+    //initialize change detection so bindings are updated
+    fixture.detectChanges();
+    //could use the debug element in the query function
+    expect(fixture.nativeElement.querySelector('h2').textContent).toContain('Common Name');
+  })
+
+  it('back button should return to list page', () => {
+    fixture.detectChanges();
+    let component: DetailComponent = fixture.componentInstance;
+    component.onBack();
+    expect(mockRouter.navigate).toHaveBeenCalledWith(['/list']);
+  })
+})
